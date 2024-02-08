ba4741fc9ad54dfe02e7ac77ea1e69ba082056c2
irtassedat <sedatirtas.1@gmail.com>
2024-02-08 13:48:45 +0300

    feat: Integrate ThemeProvider and LanguageProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from './context/ThemeContext'; // ThemeProvider import edin
+import { LanguageProvider } from './context/LanguageContext'; // LanguageProvider import edin
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider> {/* ThemeProvider ile App'i sarmalayın */}
+      <LanguageProvider> {/* LanguageProvider ile App'i sarmalayın */}
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
