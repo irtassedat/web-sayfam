ec3737284ba413562cd7704147db8046fb709bb5
irtassedat <sedatirtas.1@gmail.com>
2024-02-08 10:23:21 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Skills from "./components/Skills";
+import Profile from './components/Profile';
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import { ThemeProvider } from './context/ThemeContext';
+import { LanguageProvider } from './context/LanguageContext';
+import { DataProvider } from './context/DataContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// LoadingAnimation bileşenini import etmek gerekirse
+// import LoadingAnimation from './components/LoadingAnimation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LanguageProvider>
+        <DataProvider>
+          <ThemeProvider>
+            <Header />
+            <Skills />
+            <Profile />
+            <Projects />
+            <Footer />
+          </ThemeProvider>
+        </DataProvider>
+      </LanguageProvider>
+      <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover />
+    </>
   );
 }
 
