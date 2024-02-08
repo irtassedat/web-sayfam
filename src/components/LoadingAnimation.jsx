beb6f27bddd8eac707b51dd76837841dc3eeda81
irtassedat <sedatirtas.1@gmail.com>
2024-02-08 08:52:55 +0300

    Add LoadingAnimation component for displaying loading state

---
+++ src/components/LoadingAnimation.jsx
@@ -0,0 +1,14 @@
+// src/components/LoadingAnimation.jsx
+
+import React from 'react';
+
+const LoadingAnimation = () => {
+    return (
+        <div className="loading-container">
+            {/* Buraya özel yükleme animasyonunuzu ekleyin */}
+            <div>Yükleniyor...</div>
+        </div>
+    );
+};
+
+export default LoadingAnimation;
